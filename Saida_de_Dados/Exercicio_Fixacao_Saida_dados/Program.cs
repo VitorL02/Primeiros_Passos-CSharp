68971ede6010b31cf927f8ec2f0715863b9a3ea5
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-10-26 07:20:20 -0300

    Aprendendo a saida de variaveis em C# e realizando o exercicio de fixação utilizando intepolação de variavies e o placeHolder

---
+++ Saida_de_Dados/Exercicio_Fixacao_Saida_dados/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Fixacao_Saida_dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string produto1= "Computador";
+           string produto2 ="Mesa de escritorio";
+
+           int idade = 30;
+           int codigo = 5290;
+           char genero = 'M';
+
+            double preco1= 2100.01;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            System.Console.WriteLine("Produtos");
+            System.Console.WriteLine($"{produto1} cujo preço é $ {preco1:f2}");
+            System.Console.WriteLine($"{produto2}, cujo preço e $ {preco2:f2}");
+            System.Console.WriteLine($"Registro :{idade} anos de idade, codigo {codigo} e gênero {genero}" );
+            System.Console.WriteLine($"Medida com 8 casas decimais: {medida:f8}");
+            System.Console.WriteLine($"Arredondador (tres casa decimais): {medida:f3}");
+            System.Console.WriteLine("Separador decimal invariant culture: {0}" , medida.ToString(CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ Saida_de_Dados/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Saida_de_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            int idade = 18;
+            double saldo = 550.3068478894;
+            string nome = "Clara";
+
+            System.Console.Write("Bom dia"); //Não realiza a quebra de linha
+            System.Console.WriteLine("Bom dia!");
+            System.Console.WriteLine("Como você está?");
+
+            System.Console.WriteLine("------------");
+            System.Console.WriteLine(genero);
+            System.Console.WriteLine(idade);
+            System.Console.WriteLine(nome);
+            System.Console.WriteLine(saldo.ToString("F2"));   //Limita a somente dois caracteres o valor 
+            System.Console.WriteLine(saldo.ToString("F4"));
+            System.Console.WriteLine(saldo.ToString("F4",CultureInfo.InvariantCulture)); //Ignora todas as formatações de sistema e usa o "." como separador
+            System.Console.WriteLine("------------");
+            System.Console.WriteLine("{0} tem {1} anos e tem saldo igual {2:F2} reais", nome, idade ,saldo); // Utiliza o placeHolder para mostrar as variaveis
+            System.Console.WriteLine($"{nome} tem {idade} anos e tem o saldo igual {saldo:F2}"); //Utilizando agora a intepolação de variaveis 
+            System.Console.WriteLine(nome + " tem " + idade + " anos e tem o saldo igual a " + saldo.ToString("f2")); //Concatenação de variaveis
+        }
+    }
+}
