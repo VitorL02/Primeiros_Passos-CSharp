4341750463937af51cf04dfd58a475d6d92e69b4
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-10-27 06:49:13 -0300

    Utilizando atribuição de variaveis acumulativas em numeros e strings

---
+++ Atribuicao_de_Variaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atribuicao_de_Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int atribuicao1 = 10;
+            Console.WriteLine(atribuicao1);
+                //Vatriaveis de atribuição acumulativa,usa o valor antigo(Realiza uma soma,divisão,multiplicação etc) como o valor apos  a variavel
+            atribuicao1 +=2;
+            System.Console.WriteLine(atribuicao1);
+
+            atribuicao1 *=3;
+            System.Console.WriteLine(atribuicao1);
+
+            string atribuicao_string = "ABDC"; //Variaveis de atribuicao acumulativa funcionam em strings
+            System.Console.WriteLine(atribuicao_string);
+
+            atribuicao_string+= " Vitor";
+            System.Console.WriteLine(atribuicao_string);
+        
+        }
+    }
+}
