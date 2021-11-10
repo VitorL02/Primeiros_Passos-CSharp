1c4a4de12d8221976c2beae84045b2218b5701d4
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-11-10 07:27:06 -0300

    Aprendendo agora a saida de dados em c# e utilizando a interpolção para retornar em sequencia,utilizando vetores e função split para separar as strings e retornar 3 cores diferentes

---
+++ Entrada_de_dados/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Entrada_de_dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           String vermelho = Console.ReadLine();
+           String frase = Console.ReadLine();
+           String azul = Console.ReadLine();
+           String amarelo = Console.ReadLine();
+           String cores = Console.ReadLine();
+
+        //    String[] vetor_string = cores.Split(' '); //Split e uma função para "recortar strings"
+
+            String[] vetor_string = Console.ReadLine().Split(' '); // Realiza a mesma função de declarar uma variavel,como na linha anterior,porem economiza memoria,por não necessariamente declarar uma variavel para receber os dados.
+           String primeira_cor = vetor_string[0];
+           String segunda_cor = vetor_string[1];
+           String terceira_cor = vetor_string[2];
+
+
+
+            System.Console.WriteLine("Você digitou : ");
+           System.Console.WriteLine(frase);
+           System.Console.WriteLine(vermelho);
+           System.Console.WriteLine(azul);
+           System.Console.WriteLine(amarelo);
+           System.Console.WriteLine($"As tres cores digitadas são : {primeira_cor} , {segunda_cor}, {terceira_cor} "); // Utilizando a interpolação para retorno de dados na mesma linha
+
+        }
+    }
+}
