7d46f394f5c0b865854050e1c7b891de8088147c
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-11-11 07:37:56 -0300

    Iniciando o exercicio de fixação e apresentando os dados a serem preenchidos para o usuario

--- Entrada_de_dados/Exercicio_Entrada_dados/Program.cs
+++ Entrada_de_dados/Exercicio_Entrada_dados/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Exercicio_Entrada_dados
 {
@@ -6,7 +7,21 @@ namespace Exercicio_Entrada_dados
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Digite seu nome completo");
+            String nome = Console.ReadLine();
+            System.Console.WriteLine("Quantos quartos sua casa possui");
+            int quantidade_quartos = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Digite o preço de um produto");
+            double preco_produto = double.Parse(Console.ReadLine());
+
+            String []  dados  = Console.ReadLine().Split(" ");
+            System.Console.WriteLine("Digite em sequencia seu ultimo nome, idade , e sua altura");
+            String ultimo_nome = dados[0];
+            int idade = int.Parse(dados[1]);
+            double altura = double.Parse(dados[2],CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Os dados informados são : ");
+            
         }
     }
 }
