9822cf43f810a7a5a419c944f9fc0001865f631f
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-11-12 06:54:48 -0300

    Finalizando o exercicio de entrada de dados retornando dados entregues pelo usuario,tratando o dado double de , para . utilizando o CultureInfo.IvariantCulture

--- Entrada_de_dados/Exercicio_Entrada_dados/Program.cs
+++ Entrada_de_dados/Exercicio_Entrada_dados/Program.cs
@@ -14,14 +14,15 @@ namespace Exercicio_Entrada_dados
             System.Console.WriteLine("Digite o preço de um produto");
             double preco_produto = double.Parse(Console.ReadLine());
 
-            String []  dados  = Console.ReadLine().Split(" ");
             System.Console.WriteLine("Digite em sequencia seu ultimo nome, idade , e sua altura");
+            String []  dados  = Console.ReadLine().Split(" ");
             String ultimo_nome = dados[0];
             int idade = int.Parse(dados[1]);
             double altura = double.Parse(dados[2],CultureInfo.InvariantCulture);
 
-            System.Console.WriteLine("Os dados informados são : ");
+            System.Console.WriteLine($"Os dados informados são : \r\n Nome: {nome} \r\n Quartos na casa : {quantidade_quartos} \r\n O preço do produto é :" + preco_produto.ToString("F2",CultureInfo.InvariantCulture) +  $"\r\n Sobrenome: {ultimo_nome} \r\n  Idade : {idade}" + "\r\n Altura : " + altura.ToString("F2",CultureInfo.InvariantCulture));
             
         }
     }
 }
+
